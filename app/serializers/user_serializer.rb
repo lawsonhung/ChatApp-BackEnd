4d9916b2fe71f4b3a9a681d583e55b072acb403d
github username <github email address>
2019-09-11 09:55:17 -0400

    editted serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, 
+  attributes :id ,:name ,:online_status
+  has_many :chats
+  has_many :chat_boxes
+  has_many :chats, through: :chat_boxes
+
 end
